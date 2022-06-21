24480417fbf58b52eb2e56b605c465d76ae16ab2
chandani nonghanvadra <91254443+NonghanvadraChandani@users.noreply.github.com>
2022-06-21 10:15:21 +0530

    Update route.js

--- route.js
+++ route.js
@@ -25,4 +25,26 @@ router.post('/movies', async(req, res) => {
         }
     });
 })
+
+//for updating data
+
+router.patch('/movies/:id', async(req, res) => {
+        const iMovie = await Movie.findOne({ _id: req.params.id });
+
+        iMovie.name = req.body.name;
+        iMovie.rating = req.body.rating;
+
+        await iMovie.save((err, msg) => {
+
+            if (err) {
+                res.status(500).json({ "err": err })
+            } else {
+                res.status(200).json({ "message": msg })
+            }
+        })
+    })
+    //for deleting data
+
+
+
 module.exports = router;
